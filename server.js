8695261981c8a242dcbc6461c287d9936814470a
janglee123 <merupatel123@gmail.com>
2019-03-02 20:15:12 +0530

    bla bla bla

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
 var express = require('express');
 var bodyParser = require("body-parser");
-var app = express();
 const nlu = require('./nluService');
+var app = express();
 
-app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.static('public'));
+app.set('view engine', 'ejs')
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
 
-app.post('/login',function(req,res){
-  var req_url=req.body.url;
-  console.log("URL = "+req_url);
-  nlu.get(req_url,{},(res)=>{
-    console.log(res);
-  });
-	res.sendFile(__dirname + '/views/login.html');
-  res.end("yes");
-});
+app.get('/', function (req, res) {
+	res.render('index', { data: false });
+})
 
-var listener = app.listen(8008, function() {
-  console.log('Your app is listening on port http://localhost:' + listener.address().port);
+
+app.post('/', function (req, res0, body) {
+
+	let req_url = req.body.url;
+	nlu.get(req_url, {}, (res) => {
+		console.log(res);
+		res0.render('index', { data: true, res:JSON.stringify(res) });
+	});
+})
+
+var listener = app.listen(8008, function () {
+	console.log('Your app is listening on port http://localhost:' + listener.address().port);
 });
