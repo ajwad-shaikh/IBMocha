6b346887c6a3cd7da7b38e8f3cd0bac24698c6c5
janglee123 <merupatel123@gmail.com>
2019-03-02 17:19:25 +0530

    nlu added

---
+++ nluService.js
@@ -0,0 +1,31 @@
+const NLU = require('watson-developer-cloud/natural-language-understanding/v1.js');
+const api = require('./config.json');
+const BASE_URL = api.url;
+const API_KEY = api.key;
+const nlu = new NLU({
+    version: '2018-11-16',
+    iam_apikey: API_KEY,
+    url: BASE_URL
+});
+
+
+exports.run = function(url,features,callback){
+    let parameters = {
+        'url': 'www.ibm.com',
+        'features': {
+            'categories': {
+                'limit': 3
+            }
+        }
+    };
+    
+    nlu.analyze(parameters,(err, res)=>{
+        if(err){
+            console.error(err);
+            return;
+        }
+        console.log(res);
+    });
+}
+
+
